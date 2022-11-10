3d0711b00bf3a4026d97eae69558c92e731c1744
YEthYuan <maxwell.yuanyeh@gmail.com>
2022-11-10 10:47:28 -0800

    baidu

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,6 +8,7 @@ export default function App({ Component, pageProps }) {
         <title>Ye Yuan (Ethan) - Software Development Engineer</title>
         <meta name="description" content="Ye Yuan is an aspiring Backend/Fullstack Software Development Engineer who is mainly interested in Software Engineering, Scalable System, Research & Development, Machine Learning, and Computer Security. Ye Yuan was graduated from University of California Santa Barbara (UCSB), and Huazhong University of Science and Technology (HUST). He has been employed by Lenovo, and GaTech." />
         <meta name="google-site-verification" content="YfRqgo2_r9MMEz19CPbJg4tVBSL-Cu9BERH1uOt7-1M" />
+        <meta name="baidu-site-verification" content="code-zJHKTZtk10" />
       </Head>
       <Theme>
         <Component {...pageProps} />
